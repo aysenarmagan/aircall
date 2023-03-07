224238669451b894b39a7dae47b0db741a7a3724
Aysen Armagan <aysen.armagan@prodigygame.com>
2023-03-07 02:45:53 -0500

    Add files via upload

--- src/ActivityFeed.jsx
+++ src/ActivityFeed.jsx
@@ -14,7 +14,7 @@ const ActivityFeed = ({
 
   useEffect(() => {
     fetch(
-      "https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/activities"
+      "https://cerulean-marlin-wig.cyclic.app/activities"
     )
       .then((response) => response.json())
       .then((data) => setActivities(data))
